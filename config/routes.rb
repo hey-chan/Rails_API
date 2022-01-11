29ca1e24e61c3d4d96cc96be4702f1c1a599acb2
RuilaiZhang <Ray.zhangruilai@gmail.com>
2022-01-11 20:49:30 +1100

    separate comments

--- config/routes.rb
+++ config/routes.rb
@@ -11,33 +11,33 @@ Rails.application.routes.draw do
   get "/parks", to: "parks#index", as: "parks"
 
   # Displays one park based on park.id
-  get "/parks/:id", to: "parks#show", as: "park"
+  get "/parks/:park_id", to: "parks#show", as: "park"
 
   # POST a park (this will be restricted to ONLY admin)
   post "/parks", to: "parks#create"
 
   # UPDATE a park (again restricted to admin)
-  put "/parks/:id", to: "parks#update"
-  patch "/parks/:id", to: "parks#update"
+  put "/parks/:park_id", to: "parks#update"
+  patch "/parks/:park_id", to: "parks#update"
 
   # DELETE a park (restricted to admin)
-  delete "/parks/:id", to: "parks#destroy"
+  delete "/parks/:park_id", to: "parks#destroy"
 
   ### Comments connected to each part
-  get "/parks/:id/comments", to: "posts#index", as: "reviews"
+  get "/parks/:park_id/comments", to: "posts#index", as: "reviews"
 
   ### STILL work in progress
-  get "/parks/:id/comments/:id", to: "posts#show", as: "review"
+  get "/parks/comments/:id", to: "posts#show", as: "review"
 
   # CREATE a comment for a park
-  post "/parks/:id/comments", to: "posts#create"
+  post "/parks/:park_id/comments", to: "posts#create"
 
   # UPDATE a comment for park
-  put "/parks/:id/comments", to: "posts#update"
-  patch "/parks/:id/comments", to: "posts#update"
+  put "/parks/:park_id/comments", to: "posts#update"
+  patch "/parks/:park_id/comments", to: "posts#update"
 
   # DELETE a comment for park
-  delete "/parks/:id/comments", to: "posts#destroy"
+  delete "/parks/:park_id/comments", to: "posts#destroy"
 
   ### Authentication routing
   # Login with a user
